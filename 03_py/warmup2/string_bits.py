a5860f269874a3aa745fec4f631615dc91922993
Nicole <nzhou30@stuy.edu>
2022-09-21 21:57:44 -0400

    codingbat python

--- 03_py/warmup2/string_bits.py
+++
@@ -1,9 +0,0 @@
-# Given a string, return a new string made of every other char starting with
-# the first, so "Hello" yields "Hlo".
-
-def string_bits(str):
-  return str[::2]
-
-string_bits('Hello') # 'Hlo'
-string_bits('Hi') # 'H'
-string_bits('Heeololeo') # 'Hello'
