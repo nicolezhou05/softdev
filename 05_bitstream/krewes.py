18763d2d06694960b40fae883d818795e70da2f4
nicole <nzhou30@stuy.edu>
2022-09-29 13:20:03 -0400

    05 time

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,23 @@
+# 
+# SoftDev
+# K05
+# 2022-09-29
+# time spent: 
+
+'''
+DISCO:
+1)
+
+QCC:
+1)
+
+OPS SUMMARY:
+1)
+
+'''
+
+# def something(file):
+#     krewes_file = open(file, "r")
+#     content = krewes_file.read()
+#     separated = content.split("@@@")
+#
