0fe5a7e258210b5953b13db8f63391ee26691f5e
Nicole <nzhou30@stuy.edu>
2022-10-09 21:22:55 -0400

    more into flask (debug and new lin)

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -1,22 +1,34 @@
 # Polymer Erasers
 # SoftDev
+# K08 -- Putting it Together -- flask, debug, new line
 # 2022-10-06
+# time spent: 0.8hrs
 
 '''
 DISCO:
-0) 
+ - It appears like only one function is run in the entire app and when you call other functions, their return values don't show up on the screen
+ - To create line breaks, we need to use "<br>", in quotations. It does not literally print out <br> which we thought it would
+ - app.debug was extremely helpful so that we would not need to start and stop our app every time we made an edit
+
+QCC:
+ - Is there a more efficient way to put information on the page rather than using string concatenation?
+ - What is flask?
+ - How can we incorporate other apps with different names? This one is named __main__ by default. Can we name other apps different names and interweave them?
+ - How can we incorporate different functions?
 '''
 
 import csv
 import random
 
-
-heading = "abc "
 from flask import Flask
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
 def random_occupation():
+    heading = """
+    Polymer Erasers: Nicole Zhou, Samantha Hua, and Russell Goychayev
+    """
+
     # declare an empty dictionary
     occupations = {}
 
@@ -35,15 +47,15 @@ def random_occupation():
             # divide by total to get a percentage
             occupations[key] = occupations[key]/total
         #print(occupations)
-        occupations["Total"] = 0
-        return heading + str(random.choices(list(occupations.keys()), weights = occupations.values()))
-    
+        occupations.pop("Total")
 
-#print(random_occupation("occupations.csv"))
+    # to get all of the occupations
+    list_of_occs = "<br>"
+    for occupation in occupations:
+        list_of_occs += occupation + "<br>"
 
-# if __name__ == "__main__":  # true if this file NOT imported
-#     app.debug = True        # enable auto-reload upon code change
-#     app.run()
-app.run()
-    
-# Our prediction is that after we change the code, save, then reload, the website will reflect the changes
+    return heading + "<br>" + "<br>" + "List of occupations: " + list_of_occs + "<br>" + "Random occupation: "+ str(random.choices(list(occupations.keys()), weights = occupations.values())[0])
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
