64dd434a59b387e061f4c7c41956c6b67551ba92
Nicole <nzhou30@stuy.edu>
2022-10-25 21:38:04 -0400

    progressing

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -1,7 +1,8 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Oct 2022
+# JANitors: Aden Garbutt, Jing Yi Feng, Nicole Zhou
+# SoftDev
+# K18 -- (Python+SQLite)3: A Mare Widge Made in Hebben
+# 2022-10-25
+# time spent: 0.5 hrs (so far)
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -16,18 +17,32 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
-csvfile = open('courses.csv', 'r')
-reader = csv.DictReader(csvfile)
-command = "create table courses(code, mark, id)"
-for row in reader:
-    command += "insert into tb1 values(" + row['code'] , row['mark'], row['id'] + ")"
-    .execute(command)
+
+# command = 'create table courses (code text, mark int, id int);'
+# c.execute(command)
+
+with open('courses.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        str = row['code'] + "'," + row['mark'] + "," + row['id']
+        command = "insert into courses values('" + str + ");"
+        c.execute(command)
+       #print(row['code'], row['id'])
+        
+c.execute("select * from courses;")
+
+# csvfile = open('courses.csv', 'r')
+# reader = csv.DictReader(csvfile)
+# for row in reader:
+#     #command = 'insert'
+#     command = 'insert into courses values(' + row['code'], row['mark'], row['id'] + ');'
+#     #c.execute(command)
+#     print(command)
 
 #command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+#c.execute(command)    # run SQL statement
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
